e287d733c87fd78dc55fd8d4e1961d0575eeda57
gudy <1242018755@qq.com>
2018-10-08 21:13:48 +0800

    Update black and white block.cpp
    根据定位要求更改方块定位

--- black and white block.cpp
+++ black and white block.cpp
@@ -12,15 +12,15 @@ int main()
 	//图像的宽度和高度
 	int imgXlen, imgYlen;
 	//输入图像路径
-	char* srcPath = "input.jpg";
+	char* srcPath = "1.jpg";
 	//输出图像的路径
 	char* dstPath = "output.tif";
 	//图像内存存储
-	GByte* buffTmp,*buffTmp2,*buffTmp3;
+	GByte* buffTmp,*buffTmp1,*buffTmp2;
 	//图像波段数
 	int i, bandNum;
 	//处理点横坐标，处理点纵坐标，处理宽度，处理高度
-	int StartX,StartY,tmpXlen,tmpYlen;
+	int StartX1,StartY1,StartX2,StartY2,tmpXlen1,tmpYlen1,tmpXlen2,tmpYlen2;
 
 	//注册驱动
 	GDALAllRegister();
@@ -46,20 +46,29 @@ int main()
         更改特定区域像素
     */
     //设定初始值
-    StartX = 60;
-    StartY = 60;
-    tmpXlen = 50;
-    tmpYlen = 55;
+    StartX1 = 300;
+    StartY1 = 300;
+    tmpXlen1 = 100;
+    tmpYlen1 = 50;
+    StartX2 = 500;
+    StartY2 = 500;
+    tmpXlen2 = 50;
+    tmpYlen2 = 100;
     //分配内存
-    buffTmp2 = (GByte*) CPLMalloc(tmpXlen * tmpYlen * sizeof(GByte));
-    buffTmp3 = (GByte*) CPLMalloc(tmpXlen * tmpYlen * sizeof(GByte));
+    buffTmp1 = (GByte*) CPLMalloc(tmpXlen1 * tmpYlen1 * sizeof(GByte));
+    buffTmp2 = (GByte*) CPLMalloc(tmpXlen2 * tmpYlen2 * sizeof(GByte));
     //对每个波段赋值255和0，对应纯白和纯黑
-    for(int i = 0;i < tmpYlen;i++){
-        for(int j = 0;j < tmpXlen;j++){
-            buffTmp2[i * tmpXlen + j] = (GByte)255;
-            buffTmp3[i * tmpXlen + j] = (GByte)0;
+    for(i = 0;i < tmpYlen1;i++){
+        for(int j = 0;j < tmpXlen1;j++){
+            buffTmp1[i * tmpXlen1 + j] = (GByte)255;
         }
     }
+    for(i = 0;i < tmpYlen2;i++){
+        for(int j = 0;j < tmpXlen2;j++){
+            buffTmp2[i * tmpXlen2 + j] = (GByte)0;
+        }
+    }
+
     //一个一个波段输入。一个一个波段输出
 	for(int i = 1 ;i <= bandNum;i++){
         poSrcDS->GetRasterBand(i)->RasterIO(GF_Read,
@@ -68,19 +77,20 @@ int main()
             0,0,imgXlen,imgYlen,buffTmp,imgXlen,imgYlen,GDT_Byte,0,0);
         //填充黑色方块
         poDstDS->GetRasterBand(i)->RasterIO(GF_Write,
-                                          StartX-50,StartY-50,tmpXlen,tmpYlen,buffTmp3,tmpXlen,tmpYlen,GDT_Byte,0,0);
+                                          StartX1,StartY1,tmpXlen1,tmpYlen1,buffTmp1,tmpXlen1,tmpYlen1,GDT_Byte,0,0);
         //填充白色方块
         poDstDS->GetRasterBand(i)->RasterIO(GF_Write,
-                                          StartX+50,StartY+50,tmpXlen,tmpYlen,buffTmp2,tmpXlen,tmpYlen,GDT_Byte,0,0);
+                                          StartX2,StartY2,tmpXlen2,tmpYlen2,buffTmp2,tmpXlen2,tmpYlen2,GDT_Byte,0,0);
         printf("... ... band %d processing ... ...\n",i);
 	}
-    //红色数据写入poDstDS
+/*    //红色数据写入poDstDS
     poDstDS->GetRasterBand(1)->RasterIO(GF_Write,
-                                        StartX,StartY,tmpXlen,tmpYlen,buffTmp2,tmpXlen,tmpYlen,GDT_Byte,0,0);
+                                        StartX,StartY,tmpXlen,tmpYlen,buffTmp1,tmpXlen,tmpYlen,GDT_Byte,0,0);
+	*/
 	//清除内存
     CPLFree(buffTmp);
+    CPLFree(buffTmp1);
     CPLFree(buffTmp2);
-    CPLFree(buffTmp3);
     //关闭dataset
     GDALClose(poDstDS);
     GDALClose(poSrcDS);
